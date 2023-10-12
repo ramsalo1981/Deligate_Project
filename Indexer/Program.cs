bff4e8eed000922cbf5f8c30c66b11d738e7e96c
ramsalo1981 <rami.salloum@hotmail.com>
2023-10-13 01:21:10 +0200

    first commit

---
+++ Indexer/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Indexer;
+using System.Xml.Serialization;
+
+var emps = new Employee[]
+{
+    new Employee {Id=1, Name="Issam A", Gender="M", TotalSales=65000m},
+    new Employee {Id=2, Name="Reem S", Gender="F", TotalSales=50000m},
+    new Employee {Id=3, Name="Suzan B", Gender="F", TotalSales=65000m},
+    new Employee {Id=4, Name="Sara A", Gender="F", TotalSales=40000m},
+    new Employee {Id=5, Name="Salah C", Gender="M", TotalSales=42000m},
+    new Employee {Id=6, Name="Rateb A", Gender="M", TotalSales=30000m},
+    new Employee {Id=7, Name="Abeer C", Gender="F", TotalSales=16000m},
+    new Employee {Id=8, Name="Marwan M", Gender="M", TotalSales=15000m} ,
+};
+
+Report report = new Report();
+
+//----------------------------(1)-----------------
+//report.ProcessEmployeeWithSalesPlus60000(emps);
+//report.ProcessEmployeeWithSalesBetween30000And59999(emps);
+//report.ProcessEmployeeWithSalesLessThan30000(emps);
+
+//----------------------------(2)-----------------
+//report.ProcessEmployee(emps, " Sales >=60 000", IsGreaterThan60k);
+//report.ProcessEmployee(emps, " Sales between 30k and 59999", IsBetween30kAnd59999);
+//report.ProcessEmployee(emps, " Sales less than 30k", IsLessThan30k);
+
+//----------------------------(3)-----------------
+//report.ProcessEmployee(emps, " Sales >=60 000", delegate (Employee e) { return e.TotalSales >= 60000; });
+//report.ProcessEmployee(emps, " Sales between 30k and 59999", delegate (Employee e) { return e.TotalSales >= 30000 && e.TotalSales <= 59999; });
+//report.ProcessEmployee(emps, " Sales less than 30k", delegate (Employee e) { return e.TotalSales < 30000; });
+
+//----------------------------(4)-----------------
+//report.ProcessEmployee(emps, " Sales >=60 000",  (Employee e) => e.TotalSales >= 60000);
+//report.ProcessEmployee(emps, " Sales between 30k and 59999",  (Employee e) => e.TotalSales >= 30000 && e.TotalSales <= 59999);
+//report.ProcessEmployee(emps, " Sales less than 30k", (Employee e) => e.TotalSales < 30000);
+
+//----------------------------(5)-----------------
+report.ProcessEmployee(emps, " Sales >=60 000",  e => e.TotalSales >= 60000);
+report.ProcessEmployee(emps, " Sales between 30k and 59999",  e => e.TotalSales >= 30000 && e.TotalSales <= 59999);
+report.ProcessEmployee(emps, " Sales less than 30k",  e => e.TotalSales < 30000);
+Console.ReadKey();
+
+//----------------------------(2)-----------------
+//static bool IsGreaterThan60k(Employee e) => e.TotalSales >= 60000;
+//static bool IsBetween30kAnd59999(Employee e) => e.TotalSales >= 30000 && e.TotalSales <=59999;
+//static bool IsLessThan30k(Employee e) => e.TotalSales < 30000;
---
+++ Indexer/Report.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    public class Report
+    {
+        public delegate bool IsIllegate(Employee e);
+        public void ProcessEmployee(Employee[] employees, string title, IsIllegate isIllegate )
+        {
+            Console.WriteLine(title);
+            Console.WriteLine("====================================");
+
+            foreach (var e in employees)
+            {
+                if (isIllegate(e))
+                {
+                    Console.WriteLine($"{e.Id} | {e.Name} | {e.Gender} | {e.TotalSales}");
+                }
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        //----------------------------(1)-----------------
+        //public void ProcessEmployeeWithSalesPlus60000(Employee[] employees)
+        //{
+        //    Console.WriteLine("Employee with sales over 60 000: ");
+        //    Console.WriteLine("====================================");
+
+        //    foreach (var e in employees)
+        //    {
+        //        if (e.TotalSales >= 60000)
+        //        {
+        //            Console.WriteLine($"{e.Id} | {e.Name} | {e.Gender} | {e.TotalSales}");
+        //        }
+        //    }
+        ////    Console.WriteLine("\n\n");
+        //}
+
+        //public void ProcessEmployeeWithSalesBetween30000And59999(Employee[] employees)
+        //{
+        //    Console.WriteLine("Employee with sales Between 30 000 And 59 999: ");
+        //    Console.WriteLine("====================================");
+
+        //    foreach (var e in employees)
+        //    {
+        //        if (e.TotalSales >= 30000 && e.TotalSales <=59999)
+        //        {
+        //            Console.WriteLine($"{e.Id} | {e.Name} | {e.Gender} | {e.TotalSales}");
+        //        }
+        //    }
+        ////    Console.WriteLine("\n\n");
+
+        //}
+
+        //public void ProcessEmployeeWithSalesLessThan30000(Employee[] employees)
+        //{
+        //    Console.WriteLine("Employee with sales LessThan 30 000: ");
+        //    Console.WriteLine("====================================");
+
+        //    foreach (var e in employees)
+        //    {
+        //        if (e.TotalSales < 30000)
+        //        {
+        //            Console.WriteLine($"{e.Id} | {e.Name} | {e.Gender} | {e.TotalSales}");
+        //        }
+        //    }
+        ////    Console.WriteLine("\n\n");
+
+        //}
+    }
+}
